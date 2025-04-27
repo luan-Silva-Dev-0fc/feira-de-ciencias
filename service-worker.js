a10d2391457739ec7dc55d3b30cc5fe2f6500350
LUAN SILVA <luansilva8521@outlook.com.br>
2025-04-27 19:08:10 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,27 +4,42 @@ const urlsToCache = [
   "/feira-de-ciencias/tela-principal.html",
   "/feira-de-ciencias/sem-conexao.html",
   "/feira-de-ciencias/manifest.json",
-  "/feira-de-ciencias/assets/logo.png" // Adicionando logo ao cache
+  "/feira-de-ciencias/assets/logo.png"
 ];
 
-// Instala o Service Worker e guarda os arquivos
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
         return cache.addAll(urlsToCache);
       })
+      .then(() => self.skipWaiting())
   );
 });
 
-// Tenta buscar online, se não conseguir, pega do cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request)
-        .then(response => {
-          return response || caches.match('/feira-de-ciencias/sem-conexao.html');
-        });
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request)
+          .then(response => {
+            return response || caches.match('/feira-de-ciencias/sem-conexao.html');
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
